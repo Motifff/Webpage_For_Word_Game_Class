416d862fe378249ec7894a93b82e23e5c77f3742
Motiff <lqcyt20010122@gmail.com>
2020-04-21 19:53:12 +0800

    Initial commit

---
+++ game.js
@@ -0,0 +1,32 @@
+function preload(){
+
+}
+
+function gui() {
+
+}
+
+function pocket(){
+
+}
+
+function missions(){
+    this.mission1=function(){
+
+    }
+}
+
+function scenes(){
+    this.scene1=function(){
+
+    }
+}
+let scene=0;
+function setup(){
+    createCanvas(1920,1080);
+}
+
+function draw(){
+    scenes(scene);
+
+}
