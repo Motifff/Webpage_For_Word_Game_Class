d6fcf6da1feb42d23ba7b87bf5e91571af8d121f
Motifff <lqcyt20010122@gmail.com>
2020-04-22 13:17:46 +0800

    framework

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
-let items=[[],[],[]];
+let items=[[],[],[],[]];
 let itemword=[[],[]];
 let imagebed=[[],[]];
 let hold=-1;
+let noiseback;
 
 function preload(){
 
@@ -28,7 +29,9 @@ function pockets(){
             for(let i=0;i<printlist.length;i++) {
                 if (abs(mouseX - i%8*80+50)<25 && abs(mouseY-int(i/8)*80+50)<25 ){
                     push();
+                    rotate(PI/8);
                     image(imagebed[printlist[i]][items[printlist[i]][1]], i % 8 * 80 + 50, int(i / 8) * 80 + 50, 60, 60);
+                    text(itemword[printlist[i]][0],i % 8 * 80 + 50, int(i / 8) * 80 + 80);
                     pop();
                     if (mouseIsPressed){
                         hold=printlist[i];
@@ -42,13 +45,32 @@ function pockets(){
             scene=scenetmp;
         }
     }
-
-
-
 }
+function words(_n){
+    let s=itemword[items[_n][1]];
+    for(let i=0;i<int(s.length/10)+1;i++){
+        let ss=[];
+        for(let j=i;j<10;j++){
+            ss=ss+s[j];
+        }
+        text(ss,1500,int(i/8)*20+200);
+    }
+}
+
+function introductions(_n){
+    image(noiseback,0,0);
+    image(imagebed[_n][items[_n][1]],500,width/2,400,400);
+    words(_n);
+}
+
 //mission control
 function missions(){
+    if(scene=0){
 
+    }
+    if(scene=1){
+
+    }
 }
 
 //scene below
@@ -59,7 +81,10 @@ function scenes(_scene){
             relativepos();
             scene0(reX);
         case 1:
+            relativepos();
             scene1(reX);
+        case -1:
+
     }
 }
 function relativepos(){
@@ -73,9 +98,11 @@ function relativepos(){
 
 function scene0(_x){
     pockets();
+    let things=[[],[],[]];//all things with x and y
 }
 function scene1(_x){
     pockets();
+    let things=[[],[],[]];//all things with x and y
 }
 
 
@@ -85,7 +112,6 @@ function scene1(_x){
 let scene=0;
 function setup(){
     createCanvas(1920,1080);
-    imageMode(CENTER);
 }
 
 function draw(){
